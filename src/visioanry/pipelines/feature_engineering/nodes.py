de6411b420126eebb14d67c19940e2fbca1161f3
G0rg0ne <reda1999nox@gmail.com>
2025-12-08 23:25:40 +0100

    feat: :sparkles: implement feature engineering function and integrate into pipeline

--- src/visioanry/pipelines/feature_engineering/nodes.py
+++ src/visioanry/pipelines/feature_engineering/nodes.py
@@ -17,5 +17,34 @@ def aggegate_sales_data(sales_data: pd.DataFrame, stores_info: pd.DataFrame) -> 
     stores_info['Store'] = stores_info['Store'].astype(str)
     sales_data['Store'] = sales_data['Store'].astype(str)
     aggregated_sales_data = sales_data.merge(stores_info, on='Store', how='left')
+    return aggregated_sales_data
+
+
+def feature_engineering(aggregated_sales_data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Perform feature engineering on the aggregated sales data.
+
+    Args:
+        aggregated_sales_data (pd.DataFrame): Aggregated sales data.
+    """
+    #format the date column to be a date
+    aggregated_sales_data['Date'] = pd.to_datetime(aggregated_sales_data['Date'])
+    #add a column for the day of the week
+    aggregated_sales_data['DayOfWeek'] = aggregated_sales_data['Date'].dt.dayofweek
+    #add a column for the month
+    aggregated_sales_data['Month'] = aggregated_sales_data['Date'].dt.month
+    #add a column for the year
+    aggregated_sales_data['Year'] = aggregated_sales_data['Date'].dt.year
+    #add a column for the day of the month
+    aggregated_sales_data['DayOfMonth'] = aggregated_sales_data['Date'].dt.day
+    #add a column for the day of the year
+    aggregated_sales_data['DayOfYear'] = aggregated_sales_data['Date'].dt.dayofyear
+    #one hot encode StoreType
+    aggregated_sales_data = pd.get_dummies(aggregated_sales_data, columns=['StoreType'])
+    #one hot encode Assortment
+    aggregated_sales_data = pd.get_dummies(aggregated_sales_data, columns=['Assortment'])
+    #one hot encode StateHoliday
+    aggregated_sales_data = pd.get_dummies(aggregated_sales_data, columns=['StateHoliday'])
+
     import pdb; pdb.set_trace()
     return aggregated_sales_data
--- src/visioanry/pipelines/feature_engineering/pipeline.py
+++ src/visioanry/pipelines/feature_engineering/pipeline.py
@@ -7,6 +7,7 @@ from kedro.pipeline import Node, Pipeline  # noqa
 
 from .nodes import (
     aggegate_sales_data,
+    feature_engineering,
 )
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -17,6 +18,12 @@ def create_pipeline(**kwargs) -> Pipeline:
                 inputs=["sales_train_data", "stores_info"], 
                 outputs="aggregated_sales_data", 
                 name="aggegate_sales_data",
+                ),
+            Node(
+                feature_engineering,
+                inputs="aggregated_sales_data",
+                outputs="feature_engineered_sales_data",
+                name="feature_engineering",
                 ),
         ]
     )
