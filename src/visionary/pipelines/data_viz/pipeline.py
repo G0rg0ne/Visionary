edf57ef79e056b41b000b8544872ad89885c1588
G0rg0ne <reda1999nox@gmail.com>
2026-01-10 23:48:14 +0100

    feat: add boilerplate for data visualization pipeline, including configuration, nodes, and pipeline structure

---
+++ src/visionary/pipelines/data_viz/pipeline.py
@@ -0,0 +1,10 @@
+"""
+This is a boilerplate pipeline 'data_viz'
+generated using Kedro 1.1.1
+"""
+
+from kedro.pipeline import Node, Pipeline  # noqa
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return Pipeline([])
