a439d22d80580696593f2347a128e5a1896bfd8d
G0rg0ne <reda1999nox@gmail.com>
2026-01-17 22:01:18 +0100

    feat: integrate feature engineering pipeline into the pipeline registry for enhanced data processing

--- src/visionary/pipeline_registry.py
+++ src/visionary/pipeline_registry.py
@@ -6,9 +6,9 @@ from kedro.pipeline import Pipeline
 
 from .pipelines.data_ingestion import pipeline as data_ingestion_pipe
 from .pipelines.feature_engineering import pipeline as feature_engineering_pipe
-from .pipelines.model_training import pipeline as model_training_pipe
 from .pipelines.data_preparation import pipeline as data_preparation_pipe
 from .pipelines.data_viz import pipeline as data_viz_pipe
+
 
 def register_pipelines() -> dict[str, Pipeline]:
     """Register the project's pipelines.
@@ -21,6 +21,7 @@ def register_pipelines() -> dict[str, Pipeline]:
     data_ingestion_pipeline = data_ingestion_pipe.create_pipeline()
     data_preparation_pipeline = data_preparation_pipe.create_pipeline()
     data_viz_pipeline = data_viz_pipe.create_pipeline()
+    feature_engineering_pipeline = feature_engineering_pipe.create_pipeline()
 
 
     #create the ML pipeline
@@ -32,5 +33,6 @@ def register_pipelines() -> dict[str, Pipeline]:
         "ingestion_pipeline": data_ingestion_pipeline,
         "fusion_pipeline": data_preparation_pipeline,
         "viz_pipeline": data_viz_pipeline,
+        "fe_pipeline": feature_engineering_pipeline,
         #"training_pipeline": training_pipeline,
     }
