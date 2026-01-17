aa26d07b3053d6e787c66e66f4dc58310e5b2b3f
G0rg0ne <reda1999nox@gmail.com>
2026-01-17 22:01:12 +0100

    feat: implement feature engineering updates to include holiday data and streamline data processing

--- src/visionary/pipelines/feature_engineering/nodes.py
+++ src/visionary/pipelines/feature_engineering/nodes.py
@@ -3,87 +3,23 @@ This is a boilerplate pipeline 'feature_engineering'
 generated using Kedro 1.1.1
 """
 import pandas as pd
+from loguru import logger
+from holidays import CountryHoliday
+from datetime import datetime
 
-def aggegate_sales_data(sales_data: pd.DataFrame, stores_info: pd.DataFrame) -> pd.DataFrame:
-    """
-    Aggregate the sales data by date and store.
+def fix_data_types(merged_data: pd.DataFrame) -> pd.DataFrame:
+    merged_data['query_date'] = pd.to_datetime(merged_data['query_date'])
+    merged_data['departure_date'] = pd.to_datetime(merged_data['departure_date'])
+    return merged_data
 
-    Args:
-        sales_data (pd.DataFrame): Training sales data.
-        stores_info (pd.DataFrame): Stores information.
-    """
-    #format the date column to be a date
-    sales_data['Date'] = pd.to_datetime(sales_data['Date'])
-    stores_info['Store'] = stores_info['Store'].astype(str)
-    sales_data['Store'] = sales_data['Store'].astype(str)
-    aggregated_sales_data = sales_data.merge(stores_info, on='Store', how='left')
-    return aggregated_sales_data
+def add_holidays(merged_data: pd.DataFrame, airport_country_mapping: dict) -> pd.DataFrame:
+    merged_data['origin_country'] = merged_data['origin'].map(airport_country_mapping)
+    merged_data['destination_country'] = merged_data['destination'].map(airport_country_mapping)
+    merged_data['origin_departure_holidays'] = merged_data.apply(lambda row: CountryHoliday(row['origin_country'], years=row['departure_date'].year).get(row['departure_date']), axis=1)
+    merged_data['destination_departure_holidays'] = merged_data.apply(lambda row: CountryHoliday(row['destination_country'], years=row['departure_date'].year).get(row['departure_date']), axis=1)
+    return merged_data
 
-def extract_holidays(sales_data: pd.DataFrame) -> pd.DataFrame:
-    """
-    Extract holidays from the sales data, including both state and school holidays.
-
-    Args:
-        sales_data (pd.DataFrame): Sales data.
-    """
-    holiday_data = sales_data[['Date', 'StateHoliday','SchoolHoliday']].copy()
-    holiday_data = holiday_data.drop_duplicates()
-    holiday_data['StateHoliday'] = holiday_data['StateHoliday'].astype(str)
-    holiday_data['SchoolHoliday'] = holiday_data['SchoolHoliday'].astype(str)
-
-    # Select rows where there is either a StateHoliday or a SchoolHoliday
-    #holiday_data = holiday_data[(holiday_data['StateHoliday'] != '0') | (holiday_data['SchoolHoliday'] != '0')]
-    holiday_data = holiday_data[(holiday_data['StateHoliday'] != '0')]
-
-    holiday_map = {
-        'a': 'PublicHoliday',
-        'b': 'Easter',
-        'c': 'Christmas'
-    }
-
-    # For state holidays, assign mapped name if present
-    holiday_data['holiday'] = holiday_data['StateHoliday'].map(holiday_map)
-
-    # Where there is a school holiday, add/overwrite to 'SchoolHoliday'
-    #holiday_data.loc[holiday_data['SchoolHoliday'] != '0', 'holiday'] = 'SchoolHoliday'
-    prophet_holidays = holiday_data[['Date', 'holiday']].rename(columns={'Date': 'ds'})
-    return prophet_holidays
-
-def split_data_dates(sales_data: pd.DataFrame, split_date: str) -> pd.DataFrame:
-    """
-    Split the sales data into training and testing sets.
-    """
-    sales_data['Date'] = pd.to_datetime(sales_data['Date'])
-    sales_data = sales_data.sort_values(by='Date')
-    split_date = pd.to_datetime(split_date)
-
-    sales_data_training = sales_data[sales_data['Date'] < split_date]
-    sales_data_testing = sales_data[sales_data['Date'] >= split_date]
-
-    return sales_data_training, sales_data_testing
-
-
-def feature_engineering(sales_data: pd.DataFrame, split_date: str) -> pd.DataFrame:
-    """
-    Perform feature engineering on the aggregated sales data.
-
-    Args:
-        aggregated_sales_data (pd.DataFrame): Aggregated sales data.
-    """
-    
-    store_holidays = extract_holidays(sales_data)
-
-    #filter data for the store id to train
-    sales_data = sales_data[sales_data['Open'] == 1].copy()
-    sales_data = sales_data.drop(columns=['Customers'])
-
-
-    sales_data_training, sales_data_testing = split_data_dates(sales_data, split_date)
-
-    store_train = sales_data_training.copy()
-    store_evaluation = sales_data_testing.copy()
-
-    df_train_prophet = store_train.rename(columns={'Date': 'ds', 'Sales': 'y'})
-    future_evaluation = store_evaluation.rename(columns={'Date': 'ds'})
-
-    return df_train_prophet, future_evaluation, store_holidays
+def feature_engineering(merged_data: pd.DataFrame, airport_country_mapping: dict) -> pd.DataFrame:
+    merged_data = fix_data_types(merged_data)
+    merged_data = add_holidays(merged_data, airport_country_mapping)
+    return merged_data
--- src/visionary/pipelines/feature_engineering/pipeline.py
+++ src/visionary/pipelines/feature_engineering/pipeline.py
@@ -6,23 +6,16 @@ generated using Kedro 1.1.1
 from kedro.pipeline import Node, Pipeline  # noqa
 
 from .nodes import (
-    aggegate_sales_data,
     feature_engineering,
 )
 
 def create_pipeline(**kwargs) -> Pipeline:
     return Pipeline(
         [
-            # Node(   
-            #     aggegate_sales_data, 
-            #     inputs=["sales_train_data", "stores_info"], 
-            #     outputs="aggregated_sales_data", 
-            #     name="aggegate_sales_data",
-            #     ),
             Node(
                 feature_engineering,
-                inputs=["sales_train_data","params:split_date"],
-                outputs=["df_train_prophet", "future_evaluation", "store_holidays"],
+                inputs=["merged_data", "params:airport_country_mapping"],
+                outputs="merged_data_with_holidays",
                 name="feature_engineering",
                 ),
         ]
