373b6f2bdb05184d18c6871a56366747c4e11808
G0rg0ne <reda1999nox@gmail.com>
2026-01-20 23:55:33 +0100

    feat: integrate model training pipeline into the pipeline registry for improved workflow

--- src/visionary/pipeline_registry.py
+++ src/visionary/pipeline_registry.py
@@ -8,7 +8,7 @@ from .pipelines.data_ingestion import pipeline as data_ingestion_pipe
 from .pipelines.feature_engineering import pipeline as feature_engineering_pipe
 from .pipelines.data_preparation import pipeline as data_preparation_pipe
 from .pipelines.data_viz import pipeline as data_viz_pipe
-
+from .pipelines.model_training import pipeline as model_training_pipe
 
 def register_pipelines() -> dict[str, Pipeline]:
     """Register the project's pipelines.
@@ -22,7 +22,7 @@ def register_pipelines() -> dict[str, Pipeline]:
     data_preparation_pipeline = data_preparation_pipe.create_pipeline()
     data_viz_pipeline = data_viz_pipe.create_pipeline()
     feature_engineering_pipeline = feature_engineering_pipe.create_pipeline()
-
+    model_training_pipeline = model_training_pipe.create_pipeline()
 
     #create the ML pipeline
     #feature_engineering_pipeline = feature_engineering_pipe.create_pipeline()
@@ -34,5 +34,5 @@ def register_pipelines() -> dict[str, Pipeline]:
         "fusion_pipeline": data_preparation_pipeline,
         "viz_pipeline": data_viz_pipeline,
         "fe_pipeline": feature_engineering_pipeline,
-        #"training_pipeline": training_pipeline,
+        "training_pipeline": model_training_pipeline,
     }
