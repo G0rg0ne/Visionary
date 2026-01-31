3b1310d669c41a40954e359f24df06631d5355e7
G0rg0ne <reda1999nox@gmail.com>
2026-01-31 14:23:58 +0100

    feat: add new datasets for target vector features and update pipeline to include target vector building and cleaning steps

--- src/visionary/pipelines/feature_engineering/pipeline.py
+++ src/visionary/pipelines/feature_engineering/pipeline.py
@@ -9,6 +9,8 @@ from .nodes import (
     feature_engineering,
     split_data,
     data_augmentation,
+    build_target_vector,
+    clean_target_vector,
 )
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -22,13 +24,25 @@ def create_pipeline(**kwargs) -> Pipeline:
             ),
             Node(
                 data_augmentation,
-                inputs=["merged_atda_with_holidays", "airport_data"],
+                inputs=["merged_data_with_holidays", "airport_data"],
                 outputs="full_feature_data",
                 name="data_augmentation",
             ),
             Node(
+                build_target_vector,
+                inputs=["full_feature_data", "params:horizon"],
+                outputs="features_with_target_vector",
+                name="build_target_vector",
+            ),
+            Node(
+                clean_target_vector,
+                inputs="features_with_target_vector",
+                outputs="cleaned_horizon_features_with_target_vector",
+                name="clean_target_vector",
+            ),
+            Node(
                 split_data,
-                inputs="full_feature_data",
+                inputs="cleaned_horizon_features_with_target_vector",
                 outputs=["tickets_train_data", "tickets_test_data"],
                 name="split_dataset",
             ),
